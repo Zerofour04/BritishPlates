53169668c72ab184c816ebf996e6dc86c9d2818c
Ben // Zerofour // 04 <brubiyt@gmail.com>
2025-01-06 23:29:46 +0100

    Update config

--- client.lua
+++ client.lua
@@ -5,8 +5,8 @@
 local plateStarts = {"M"} -- First letter(s) of the plate (e.g., city identifier)
 local plateLetters = {"A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 local plateYears = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99"}
-local textureFile = "nui://Kennzeichen/plates/plate.png" -- Path to main plate texture
-local normalMapFile = "nui://Kennzeichen/plates/plate_n.png" -- Path to normal map texture
+local textureFile = "nui://GermanPlates-FiveM/plates/plate.png" -- Path to main plate texture
+local normalMapFile = "nui://GermanPlates-FiveM/plates/plate_n.png" -- Path to normal map texture
 
 -- End of Configuration Section --
 
