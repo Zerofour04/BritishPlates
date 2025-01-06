7f674775ffce0fab760bb6b4395a2a22212d8706
Ben // Zerofour // 04 <brubiyt@gmail.com>
2025-01-06 23:06:18 +0100

    Convert British plates to German format
    - Added German city identifiers in plateStarts
    - Moved texture configuration to top section
    - Updated manifest with proper credits
    - Maintained original plate generation logic

--- client.lua
+++ client.lua
@@ -1,6 +1,14 @@
-local plateStarts = {"GM",  "GN", "GO", "LX", "BX"}
-local plateYears = {"02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70"}
+---------------------------
+-- Configuration Section --
+---------------------------
+
+local plateStarts = {"M"} -- First letter(s) of the plate (e.g., city identifier)
 local plateLetters = {"A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+local plateYears = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99"}
+local textureFile = "nui://Kennzeichen/plates/plate.png" -- Path to main plate texture
+local normalMapFile = "nui://Kennzeichen/plates/plate_n.png" -- Path to normal map texture
+
+-- End of Configuration Section --
 
 Citizen.CreateThread(function()
     while true do
@@ -18,10 +26,10 @@ Citizen.CreateThread(function()
 
                 if whiteSpace == 0 then
                     local plate = plateStarts[math.random(1, #plateStarts)]
+                    plate = plate .. '' .. plateLetters[math.random(1, #plateLetters)]
+                    plate = plate .. '' .. plateLetters[math.random(1, #plateLetters)]
                     plate = plate .. '' .. plateYears[math.random(1, #plateYears)]
-                    plate = plate .. ' ' .. plateLetters[math.random(1, #plateLetters)]
-                    plate = plate .. '' .. plateLetters[math.random(1, #plateLetters)]
-                    plate = plate .. '' .. plateLetters[math.random(1, #plateLetters)]
+                    plate = plate .. '' .. plateYears[math.random(1, #plateYears)]
 
                     SetVehicleNumberPlateText(vehicle, plate)
                 end
@@ -31,3 +39,24 @@ Citizen.CreateThread(function()
         Citizen.Wait(500)
     end
 end)
+
+local textureDic = CreateRuntimeTxd('duiTxd')
+local object = CreateDui(textureFile, 540, 300)
+local handle = GetDuiHandle(object)
+CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex", handle)
+
+AddReplaceTexture('vehshare', 'plate01', 'duiTxd', 'duiTex')
+AddReplaceTexture('vehshare', 'plate02', 'duiTxd', 'duiTex')
+AddReplaceTexture('vehshare', 'plate03', 'duiTxd', 'duiTex')
+AddReplaceTexture('vehshare', 'plate04', 'duiTxd', 'duiTex')
+AddReplaceTexture('vehshare', 'plate05', 'duiTxd', 'duiTex')
+
+local object2 = CreateDui(normalMapFile, 540, 300)
+local handle2 = GetDuiHandle(object2)
+CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex2", handle2)
+
+AddReplaceTexture('vehshare', 'plate01_n', 'duiTxd', 'duiTex2')
+AddReplaceTexture('vehshare', 'plate02_n', 'duiTxd', 'duiTex2')
+AddReplaceTexture('vehshare', 'plate03_n', 'duiTxd', 'duiTex2')
+AddReplaceTexture('vehshare', 'plate04_n', 'duiTxd', 'duiTex2')
+AddReplaceTexture('vehshare', 'plate05_n', 'duiTxd', 'duiTex2')
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,15 @@
 fx_version 'cerulean'
-
 game 'gta5' 
+
+name 'German Plates'
+description 'Modified version of British Plates for German license plates.'
+author 'Owen-M | Zerofour04'
 
 client_scripts {
     'client.lua'
 }
 
-name 'British Plates'
-description 'Customisable british plates.'
-author 'Owen-M'
+files {
+    'plates/plate.png',
+    'plates/plate_n.png'
+}
